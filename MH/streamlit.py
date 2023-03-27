13819c199c089896823a9a3a0998a5d039634839
MyungHyun_KIM <126433920+Myun9hyun@users.noreply.github.com>
2023-03-27 14:46:18 +0900

    pickle모듈 추가

--- MH/streamlit.py
+++ MH/streamlit.py
@@ -6,7 +6,7 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
+import pickle
 
 menu = ["메인페이지", "데이터페이지", "기타"]
 choice = st.sidebar.selectbox("메뉴를 선택해주세요", menu)
