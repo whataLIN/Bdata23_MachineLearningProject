d0fb5cac5013982bf4a6d66e0bf29729273d28b6
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-03-28 22:42:20 +0900

    소수점

--- MH/streamlit.py
+++ MH/streamlit.py
@@ -763,7 +763,8 @@ elif choice == "시뮬레이션":
         model = joblib.load(f'KL/{option}.pkl')
         # pred = model.predict(variable)
         # pred = np.round(pred, 2)
-        pred = np.round(model.predict(variable), 2)
+        pred = model.predict(variable)
+        pred = pred.round(2)
 
         st.metric("결과: ", pred)
 
