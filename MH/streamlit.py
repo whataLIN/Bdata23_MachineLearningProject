b0af39ad6d7a22591109d931ea86295366ac6cf8
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-03-28 18:29:44 +0900

    시뮬레이션

--- MH/streamlit.py
+++ MH/streamlit.py
@@ -523,22 +523,25 @@ elif choice == "시뮬레이션":
     cols = st.columns(5)
     
     player_keys = [
-        "shooting", "Dribbling", "Passing", "Rebounding", 'Defense', "Stamina"
-    ]
+        "Shooting", "Dribbling", "Rebounding", 'Defense', "Stamina"
+    ]       #"Passing"
 
-    pl=pd.DataFrame(columns=player_keys, index=range(1,6))
+    pl=pd.DataFrame(columns=player_keys, index=[f"{p}번째 선수" for p in range(1,6)])
     # for i, t in enumerate(tabs):
-    url='https://raw.githubusercontent.com/whataLIN/sportsTOoTOo/main/Basketball_processing.csv'
+    url='https://github.com/whataLIN/sportsTOoTOo/raw/main/cbb.csv'
     df = pd.read_csv(url)
+    df.drop(['TEAM', 'YEAR','W','G'],axis=1, inplace=True)
 
     conf_list=list(df['CONF'].unique())
     team_conf= st.selectbox('참가할 대회를 선택해주세요.', options=conf_list)
 
+    position_list=['센터','파워포워드','포인트가드','슈팅가드', '스몰포워드']
     for i, c in enumerate(cols):
         with c:
-            st.slider("슈팅", min_value=1, max_value=10, value=1, key=f"shooting_{i+1}")
+            st.write(position_list[i])
+            st.slider("슈팅", min_value=1, max_value=10, value=1, key=f"Shooting_{i+1}")
             st.slider("드리블", min_value=1, max_value=10, value=1, key=f"Dribbling_{i+1}")
-            st.slider("패스", min_value=1, max_value=10, value=1, key=f"Passing_{i+1}")
+            # st.slider("패스", min_value=1, max_value=10, value=1, key=f"Passing_{i+1}")
             st.slider("리바운드", min_value=1, max_value=10, value=1, key=f"Rebounding_{i+1}")
             st.slider("수비", min_value=1, max_value=10, value=1, key=f"Defense_{i+1}")
             st.slider("스테미나", min_value=1, max_value=10, value=1, key=f"Stamina_{i+1}")
@@ -551,11 +554,151 @@ elif choice == "시뮬레이션":
                 stat=state[f"{p}_{i+1}"]
                 st.write(f"{p} : {stat}")
 
-            pl.loc[i+1] = player
+            pl.loc[f"{i+1}번째 선수"] = player
 
     
-    tdf = df.drop(['TEAM', 'YEAR','W','G'], axis=1).copy()
-    max_values = [tdf[i].max() for i in tdf.columns]
+    tdf = df.drop(['POSTSEASON', 'SEED', 'CONF', 'BARTHAG','WAB'], axis=1).copy()
+    # tdf = df.drop(['TEAM', 'YEAR','W','G', 'POSTSEASON', 'SEED', 'CONF', 'BARTHAG','WAB'], axis=1).copy()
+    
+    fromShooting = tdf[['ADJOE', 'EFG_O', 'FTR', '2P_O', '3P_O']].copy()
+    fromDribbling = tdf[['TORD']].copy()
+    fromRebounding = tdf[['ORB', 'DRB']].copy()
+    fromDefense = tdf[['TOR', 'EFG_D', 'ADJDE', '2P_D', '3P_D', 'FTRD']].copy()
+    fromStamina = tdf[['ADJ_T']].copy()
+
+    plusVarlist=['ADJOE', 'EFG_O', 'FTR', '2P_O', '3P_O', 'ORB', 'TOR','ADJ_T']
+    minusVarlist=['TORD', 'EFG_D', '2P_D', '3P_D', 'FTRD', 'ADJDE', 'DRB']
+
+    pl_to_per=pd.DataFrame(
+        0,
+        columns=tdf.columns,
+        index=pl.index
+    )
+
+
+    def get_max(df):   #최대값을 구해 딕셔너리로 반환하는 함수
+        return {key: int(value) for key, value in df.max().to_dict().items()}
+
+        # ADJOE, ADJDE, EFG_O, EFG_D, TOR, TORD, ORB, DRB, FTR, FTRD, 2P_O, 2P_D, 3P_O, 3P_D, ADJ_T
+        # postseason, seed는 missed tornament.
+
+    def percentage_cal(stat_pl, final_df, df, stat):      # statlist를 하나씩 받음
+            #df는 스탯별로 영향을 주는 변수끼리 나눈거
+            #stat_pl는 선수들의 스탯 모음
+            #finaldf는 결과를 반영할 df
+            #stat는 선수 스탯을 어디서 가져올건지
+
+        columnlist=df.columns
+        addper=[]
+        subper=[]
+        max_values = get_max(df)  #df의 각 값의 max값이 딕셔너리로 반환
+
+        for i in columnlist:
+            if i in plusVarlist:
+                addper.append(i)
+            else:
+                subper.append(i)
+                final_df[i]=max_values[i]/5
+
+
+        for p in range(5):
+            for i in addper:      #df의 컬럼명을 차례로 가져옴
+                final_df.loc[f"{p+1}번째 선수", i] += (int(max_values[i])/50) * stat_pl.loc[f"{p+1}번째 선수", stat]
+
+        for p in range(5):
+            for i in subper:
+                final_df.loc[f"{p+1}번째 선수", i] -= (int(max_values[i]) / 50) * stat_pl.loc[f"{p+1}번째 선수", stat]
+
+
+    percentage_cal(pl, pl_to_per, df=fromShooting, stat='Shooting')
+    percentage_cal(pl, pl_to_per, df=fromDribbling, stat='Dribbling')
+    percentage_cal(pl, pl_to_per, df=fromRebounding, stat='Rebounding')
+    percentage_cal(pl, pl_to_per, df=fromDefense, stat='Defense')
+    percentage_cal(pl, pl_to_per, df=fromStamina, stat='Stamina')
+    
+    st.write(pl_to_per)
+    teaminfo = pd.DataFrame(
+        data=pl_to_per.sum(axis=0).values.reshape(1, 15),
+        columns=tdf.columns,
+        index=["%"])
+    st.write("team info: ", teaminfo)
+
+    df_columns = ['ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D', 'TOR', 'TORD', 'ORB',
+       'DRB', 'FTR', 'FTRD', '2P_O', '2P_D', '3P_O', '3P_D', 'ADJ_T', 'WAB',
+       'CONF_A10', 'CONF_ACC', 'CONF_AE', 'CONF_ASun', 'CONF_Amer', 'CONF_B10',
+       'CONF_B12', 'CONF_BE', 'CONF_BSky', 'CONF_BSth', 'CONF_BW', 'CONF_CAA',
+       'CONF_CUSA', 'CONF_GWC', 'CONF_Horz', 'CONF_Ind', 'CONF_Ivy',
+       'CONF_MAAC', 'CONF_MAC', 'CONF_MEAC', 'CONF_MVC', 'CONF_MWC',
+       'CONF_NEC', 'CONF_OVC', 'CONF_P12', 'CONF_Pat', 'CONF_SB', 'CONF_SC',
+       'CONF_SEC', 'CONF_SWAC', 'CONF_Slnd', 'CONF_Sum', 'CONF_WAC',
+       'CONF_WCC', 'CONF_ind', 'SEED_1.0', 'SEED_2.0', 'SEED_3.0', 'SEED_4.0',
+       'SEED_5.0', 'SEED_6.0', 'SEED_7.0', 'SEED_8.0', 'SEED_9.0', 'SEED_10.0',
+       'SEED_11.0', 'SEED_12.0', 'SEED_13.0', 'SEED_14.0', 'SEED_15.0',
+       'SEED_16.0', 'SEED_Missed Tournament', 'POSTSEASON_2.0',
+       'POSTSEASON_4.0', 'POSTSEASON_8.0', 'POSTSEASON_16.0',
+       'POSTSEASON_32.0', 'POSTSEASON_64.0', 'POSTSEASON_68.0']
+
+    df_forms = pd.DataFrame(0, columns=df_columns, index=['%'])
+    
+    df_forms['SEED_Missed Tournament']=1
+    df_forms['POSTSEASON_Missed Tournament']=1
+    df_forms[f'SEED_Missed Tournament']=1
+    df_forms[f'BARTHAG']=0.5
+    df_forms[f'CONF_{team_conf}']=1
+
+    for i in range(17): # 인덱스
+        col_name = df_columns[i]
+        if col_name=='BARTHAG' or col_name=='WAB': continue
+
+        df_forms[col_name]+=teaminfo[col_name]
+
+    st.write(df_forms)
+
+    
+    # teaminfo = pd.DataFrame(
+    #     data=pl_to_per.sum(axis=0).values.reshape(1, 15),
+    #     columns=tdf.columns,
+    #     index=["%"])
+
+    # teaminfo['CONF']=team_conf
+    # teaminfo['BARTHAG']=0.5
+    # teaminfo["POSTSEASON"]="Missed Tournament"
+    # teaminfo['SEED']='Missed Tournament'
+    # teaminfo['WAB']=0
+
+    # teaminfo = teaminfo.reindex(columns=["CONF", 'ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D', "TOR", "TORD", 'ORB', 'DRB', 'FTR', 'FTRD', '2P_O', '2P_D', '3P_O', '3P_D', 'ADJ_T', 'WAB', 'POSTSEASON', 'SEED'])
+
+    # st.write(teaminfo)
+    # st.write(teaminfo[:], df.iloc[:5])
+    # st.write(len(teaminfo.columns), len(df.columns))
+
+    # #전처리 다시
+    # df.loc[len(df)] = teaminfo
+    # ps={"R68":68,"R64":64,"R32":32,"S16":16,"E8":8,"F4":4,"2ND":2,"Champion":1}
+    # df['POSTSEASON'] = df['POSTSEASON'].map(ps)
+    # df.fillna({'POSTSEASON':'Missed Tournament'}, inplace = True)
+    # df.fillna({'SEED':'Missed Tournament'}, inplace = True)
+    # df=pd.get_dummies(df, columns=['CONF','SEED','POSTSEASON'])  
+    # df = df.tail(1)
+
+    option = st.selectbox(
+    '원하는 차트를 골라주세요',
+    ('LinearRegressor', 'RandomForest', 'DecisionTree')) #'XGBoost'
+    model_path = f"KL/{option}.pkl"
+    model = joblib.load(model_path)
+
+    st.write(option)
+
+    predict_button = st.button("예측")
+
+    if predict_button:
+        variable = df_forms
+    
+        model = joblib.load(f'KL/{option}.pkl')
+        pred = model.predict(variable)
+        st.metric("결과: ", pred)
+
+
 
     
 
@@ -564,4 +707,7 @@ elif choice == "시뮬레이션":
 
                 #슈팅 : 슈팅_i
             #데이터프레임에 선수 능력치 저장하깅
+
+
+
     
