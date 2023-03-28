817e5e77459f39d9b3e90518d625b77a76870870
JoySoon <115054565+JoySoon@users.noreply.github.com>
2023-03-28 11:18:49 +0900

    LR value error fix

--- MH/streamlit.py
+++ MH/streamlit.py
@@ -288,7 +288,7 @@ elif choice == "데이터페이지":
             predict_button = st.button("예측")
 
             if predict_button:
-                    variable1 = np.array([승리수, 경기수]*38 + [경기수])
+                    variable1 = np.array([승리수, 경기수] * 28)
                     model1 = joblib.load('MH/LRmodel.pkl')
                     pred1 = model1.predict([variable1])
                     pred1 = pred1.round(2)
