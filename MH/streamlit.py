0c5dbfdd81578c6c775d55df89e973463326b425
JoySoon <115054565+JoySoon@users.noreply.github.com>
2023-03-29 09:34:52 +0900

    RF시각화

--- MH/streamlit.py
+++ MH/streamlit.py
@@ -371,7 +371,17 @@ elif choice == "데이터페이지":
             FTR = col4.slider("자유투 수치", 0, 50)
             two_O = col5.slider("2점슛 수치", 0, 50)
             three_O = col6.slider("3점슛 수치", 0, 30)
-            
+
+            # Feature Importance 그래프
+            fig = px.bar(
+            x=df.columns[:-1],
+            y=model.feature_importances_,
+            labels={'x': '변수', 'y': '중요도'}
+            )
+            fig.update_layout(title="중요한 변수 확인", xaxis_title="변수", yaxis_title="중요도")
+
+            st.plotly_chart(fig)
+       
             predict_button = st.button("예측")
 
             if predict_button:
